892ffdb45d936440927ee052aa78b15c6adfa25d
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-18 23:05:22 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Caren';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const message = 'Welcome to Flatbook';
+const newMessage = message.toUpperCase();
+const excitedWelcomeMessage = `${newMessage}, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+console.log(excitedWelcomeMessage, welcomeMessage, shortGreeting);
